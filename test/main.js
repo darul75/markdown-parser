e6d2a051deed1da9471ec7d09172317cb4c69177
darul75 <darul75@gmail.com>
2015-01-19 14:43:58 +0100

    test + bundle browserify

--- test/main.js
+++ test/main.js
@@ -3,12 +3,36 @@ var parser = require('../src/markdown-parser');
 var assert = require("assert");
 
 describe('tests', function() {
-    describe('with header', function() {
-        it('return Netherlands nice country', function(done) {
-            parser.parse("#header1 \n#header2", function(err, result) {                
-                assert.equal(result.headings.length, 2);
-                done();
-            });
+  describe('parsing', function() {
+    it('bold', function(done) {
+        parser.parse("**b** __b__", function(err, result) {                
+            assert.equal(result.bolds.length, 2);
+            done();
         });
     });
+    it('headings', function(done) {
+        parser.parse("#header1 \n#header2", function(err, result) {                
+            assert.equal(result.headings.length, 2);
+            done();
+        });
+    });
+    it('italics', function(done) {
+        parser.parse("*iiiii*\nother\n\n *iiiii*", function(err, result) {                
+            assert.equal(result.italics.length, 2);
+            done();
+        });
+    });
+    it('list', function(done) {
+        parser.parse("- item1 \n- item2 \nother\n*item1\nitem2", function(err, result) {                
+            assert.equal(result.lists[0].length, 2);
+            done();
+        });
+    });      
+    it('headings', function(done) {
+        parser.parse("#header1 \n#header2", function(err, result) {                
+            assert.equal(result.headings.length, 2);
+            done();
+        });
+    });
+  });
 });
