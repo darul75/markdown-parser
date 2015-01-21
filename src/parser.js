7b5e88af71fef686b47bb51f502b91c8676985e2
darul75 <darul75@gmail.com>
2015-01-21 09:42:50 +0100

    fix space or tab or whatever

--- src/parser.js
+++ src/parser.js
@@ -459,6 +459,9 @@ module.exports = (function() {
                             s0 = peg$parseSection();
                             if (s0 === peg$FAILED) {
                               s0 = peg$parseOthers();
+                              if (s0 === peg$FAILED) {
+                                s0 = peg$parseSpace();
+                              }
                             }
                           }
                         }
@@ -1028,28 +1031,37 @@ module.exports = (function() {
     }
 
     function peg$parseOthers() {
-      var s0, s1, s2;
+      var s0, s1, s2, s3;
 
       s0 = peg$currPos;
-      s1 = [];
-      s2 = peg$parseAnyText2();
-      if (s2 !== peg$FAILED) {
-        while (s2 !== peg$FAILED) {
-          s1.push(s2);
-          s2 = peg$parseAnyText2();
-        }
-      } else {
-        s1 = peg$c1;
+      s1 = peg$parseSpace();
+      if (s1 === peg$FAILED) {
+        s1 = peg$c43;
       }
       if (s1 !== peg$FAILED) {
-        s2 = peg$parseSpace();
-        if (s2 === peg$FAILED) {
-          s2 = peg$c43;
+        s2 = [];
+        s3 = peg$parseAnyText2();
+        if (s3 !== peg$FAILED) {
+          while (s3 !== peg$FAILED) {
+            s2.push(s3);
+            s3 = peg$parseAnyText2();
+          }
+        } else {
+          s2 = peg$c1;
         }
         if (s2 !== peg$FAILED) {
-          peg$reportedPos = s0;
-          s1 = peg$c44(s1);
-          s0 = s1;
+          s3 = peg$parseSpace();
+          if (s3 === peg$FAILED) {
+            s3 = peg$c43;
+          }
+          if (s3 !== peg$FAILED) {
+            peg$reportedPos = s0;
+            s1 = peg$c44(s2);
+            s0 = s1;
+          } else {
+            peg$currPos = s0;
+            s0 = peg$c1;
+          }
         } else {
           peg$currPos = s0;
           s0 = peg$c1;
